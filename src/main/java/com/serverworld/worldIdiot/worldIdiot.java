509cc5e0a935aa6913df3a92889bdb5769e6329f
Paul <47947919+paul0665@users.noreply.github.com>
2020-01-10 15:56:32 +0800

    Add files via upload

---
+++ src/main/java/com/serverworld/worldIdiot/worldIdiot.java
@@ -0,0 +1,10 @@
+package com.serverworld.worldIdiot;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class worldIdiot extends Plugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Yay! It loads!");
+    }
+}
